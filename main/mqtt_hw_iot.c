adb8cb9e96604ef5b1681956bcebdd72f5578c7b
shf <shf@example.com>
2026-04-15 13:33:32 +0800

    feat(mqtts): 为华为云IoT MQTT 客户端添加证书验证支持

--- main/mqtt_hw_iot.c
+++ main/mqtt_hw_iot.c
@@ -10,6 +10,8 @@
 
 esp_mqtt_client_handle_t mqtt_handle = NULL;
 
+extern const char *hw_iot_cert;
+
 void mqtt_event_callback(void *event_handler_arg,
                          esp_event_base_t event_base,
                          int32_t event_id,
@@ -51,15 +53,30 @@ void mqtt_hw_iot_init(void)
     mqtt_cfg.credentials.client_id = HW_IOT_CLIENT_ID;
     mqtt_cfg.credentials.username = HW_IOT_USERNAME;
     mqtt_cfg.credentials.authentication.password = HW_IOT_PASSWORD;
+    mqtt_cfg.broker.verification.certificate = hw_iot_cert; // 验证证书
     mqtt_handle = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(mqtt_handle, ESP_EVENT_ANY_ID, mqtt_event_callback, NULL);
     esp_mqtt_client_start(mqtt_handle);
 }
 
-void mqtt_publish_message(const char *topic, const char *message)
-{
-    if (mqtt_handle != NULL)
-    {
-        esp_mqtt_client_publish(mqtt_handle, topic, message, strlen(message), 1, 0);
-    }
-}
+const char *hw_iot_cert = "-----BEGIN CERTIFICATE-----\n"
+"MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G"
+"A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp"
+"Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4"
+"MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG"
+"A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI"
+"hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8"
+"RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT"
+"gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm"
+"KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd"
+"QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ"
+"XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw"
+"DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o"
+"LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU"
+"RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp"
+"jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK"
+"6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX"
+"mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs"
+"Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH"
+"WD9f\n"
+"-----END CERTIFICATE-----";
