faa13e51fcd21b3d2872975839ad6dc4ec2f6235
shf <shf@example.com>
2026-04-16 13:46:34 +0800

    feat: 从命令下行的 topic 中截取 request_id

--- main/hw_iot_mqtt_topic.c
+++ main/hw_iot_mqtt_topic.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <esp_log.h>
 
 #include "hw_iot_mqtt_topic.h"
+#include "hw_iot_mqtt.h"
 
 static char topic_str[128] = {0};
 
@@ -62,4 +64,37 @@ char *hw_iot_mqtt_topic_get(hw_iot_topic_type_t type, char *device_id, char *req
         break;
     }
     return topic_str;
+}
+
+char *hw_iot_mqtt_topic_get_command_request_id(esp_mqtt_event_handle_t receive_data)
+{
+    if (!receive_data || !strstr(receive_data->topic, "sys/commands/request_id="))
+    {
+        ESP_LOGE("mqtt_hw_iot", "Invalid command topic");
+        return NULL;
+    }
+
+    ESP_LOGI("mqtt_hw_iot", "receive command, topic: %.*s", receive_data->topic_len, receive_data->topic);
+
+    char topic[96] = {0};
+    char request_id[48] = {0};
+    int copy_len = (receive_data->topic_len < sizeof(topic) - 1)
+                       ? receive_data->topic_len
+                       : sizeof(topic) - 1;       // 计算要复制的长度，确保不超过缓冲区大小
+    memcpy(topic, receive_data->topic, copy_len); // 复制主题字符串到本地缓冲区
+    topic[copy_len] = '\0';                       // 确保字符串以空字符结尾
+    char *ptr = strstr(topic, "request_id=");     // 查找请求ID字符串
+    if (ptr)
+    {
+        ptr += strlen("request_id=");              // 跳过"request_id="前缀
+        char *end = strchr(ptr, '/');              // 查找请求ID后面的斜杠，确定请求ID的结束位置
+        int len = end ? (end - ptr) : strlen(ptr); // 计算请求ID的长度，如果没有斜杠，则使用剩余字符串的长度
+        if (len < sizeof(request_id))              // 确保请求ID长度不超过缓冲区大小
+        {
+            memcpy(request_id, ptr, len); // 复制请求ID到本地缓冲区
+            request_id[len] = '\0';       // 确保字符串以空字符结尾
+            ESP_LOGI("mqtt_hw_iot", "request_id: %s", request_id);
+        }
+    }
+    return request_id;
 }
--- main/hw_iot_mqtt_topic.h
+++ main/hw_iot_mqtt_topic.h
@@ -1,15 +1,18 @@
 #ifndef HW_IOT_MQTT_TOPIC_H
 #define HW_IOT_MQTT_TOPIC_H
 
+#include <mqtt_client.h>
+
 #define DEVICE_ID "69cc7e9c6b6c4d5f8d58bd94_3c-84-27-c0-2e-6c"
 
 typedef enum
 {
     HW_IOT_TOPIC_PROPERTIES_REPORT = 0, /**< 属性上报 $oc/devices/{device_id}/sys/properties/report*/
-    HW_IOT_TOPIC_COMMAND_RESPONSE,          /**< 命令确认响应 $oc/devices/{device_id}/sys/commands/response/request_id={request_id} */
+    HW_IOT_TOPIC_COMMAND_RESPONSE,      /**< 命令确认响应 $oc/devices/{device_id}/sys/commands/response/request_id={request_id} */
     HW_IOT_TOPIC_VERSION_REPORT,        /**< 版本上报 $oc/devices/{device_id}/sys/events/up */
 } hw_iot_topic_type_t;
 
 char *hw_iot_mqtt_topic_get(hw_iot_topic_type_t type, char *device_id, char *request_id); // 获取指定类型的Topic字符串
+char *hw_iot_mqtt_topic_get_command_request_id(esp_mqtt_event_handle_t receive_data);     // 获取命令请求ID
 
 #endif // HW_IOT_MQTT_TOPIC_H
