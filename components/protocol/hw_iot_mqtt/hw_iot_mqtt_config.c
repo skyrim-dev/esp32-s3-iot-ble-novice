cc7532ae81492c1f53c0aeff60a89da6ec287758
shf <shf@example.com>
2026-04-16 13:46:34 +0800

    feat: 将证书变为文件方式引用

--- components/protocol/hw_iot_mqtt/hw_iot_mqtt_config.c
+++ components/protocol/hw_iot_mqtt/hw_iot_mqtt_config.c
@@ -3,36 +3,14 @@
 
 #include <esp_log.h>
 #include <mqtt_client.h>
-#include <mbedtls/md5.h>
-#include <mbedtls/md.h>
-#include <driver/gpio.h>
 #include <cJSON.h>
 
 #include "hw_iot_mqtt_config.h"
 #include "hw_iot_mqtt_json.h"
 #include "hw_iot_mqtt_topic.h"
 
-static const char *global_sign_root_ca_cert = "-----BEGIN CERTIFICATE-----\n"
-                                              "MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G"
-                                              "A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp"
-                                              "Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4"
-                                              "MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG"
-                                              "A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI"
-                                              "hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8"
-                                              "RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT"
-                                              "gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm"
-                                              "KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd"
-                                              "QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ"
-                                              "XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw"
-                                              "DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o"
-                                              "LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU"
-                                              "RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp"
-                                              "jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK"
-                                              "6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX"
-                                              "mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs"
-                                              "Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH"
-                                              "WD9f\n"
-                                              "-----END CERTIFICATE-----";
+extern const char _binary_cert_pem_start[] asm("_binary_cert_pem_start");
+extern const char _binary_cert_pem_end[] asm("_binary_cert_pem_end");
 
 esp_mqtt_client_handle_t mqtt_handle = NULL;
 
@@ -195,7 +173,7 @@ void hw_iot_mqtt_init(void)
     mqtt_cfg.credentials.client_id = HW_IOT_CLIENT_ID;
     mqtt_cfg.credentials.username = HW_IOT_USERNAME;
     mqtt_cfg.credentials.authentication.password = HW_IOT_PASSWORD;
-    mqtt_cfg.broker.verification.certificate = global_sign_root_ca_cert; // 验证证书
+    mqtt_cfg.broker.verification.certificate = _binary_cert_pem_start; // 验证证书
     mqtt_handle = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(mqtt_handle, ESP_EVENT_ANY_ID, mqtt_event_callback, NULL);
     esp_mqtt_client_start(mqtt_handle);
