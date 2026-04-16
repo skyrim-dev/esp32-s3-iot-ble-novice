acc03bf3721fa102988bdf157ff06ce99c73f206
shf <shf@example.com>
2026-04-16 13:46:34 +0800

    feat: 新增 Kconfig 可由用户自主配置 wifi 用户名和密码，将 sdkconfi 取消索引

--- components/network/wifi/wifi.h
+++ components/network/wifi/wifi.h
@@ -3,9 +3,11 @@
 
 #include "freertos/semphr.h"
 
-// WiFi配置
-#define WIFI_SSID "天际"
-#define WIFI_PWD "sususususu"
+// WiFi配置 (从 Kconfig 读取)
+#define WIFI_SSID      CONFIG_WIFI_SSID
+#define WIFI_PWD       CONFIG_WIFI_PASSWORD
+#define WIFI_TIMEOUT   CONFIG_WIFI_CONNECT_TIMEOUT_MS
+#define WIFI_RECONNECT CONFIG_WIFI_RECONNECT_INTERVAL_MS
 
 // 外部声明的信号量
 extern SemaphoreHandle_t wifi_connected_semaphore;
