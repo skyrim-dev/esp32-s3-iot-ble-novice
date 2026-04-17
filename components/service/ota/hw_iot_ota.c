942b2f9c6679a33eb8f56aacdd5d6d76d72eae0e
shf <shf@example.com>
2026-04-17 21:00:33 +0800

    feat: 更新ota函数

--- components/service/ota/hw_iot_ota.c
+++ components/service/ota/hw_iot_ota.c
@@ -1,1 +1,20 @@
+#include <esp_log.h>
+#include <esp_https_ota.h>
+#include <stdio.h>
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include "hw_iot_ota.h"
+
+esp_err_t hw_iot_ota_init(const char *url, hw_iot_ota_finish_callback_t cb)
+{
+
+    return ESP_OK;
+}
+
+esp_err_t hw_iot_ota_start(void)
+{
+
+    return ESP_OK;
+}
--- components/service/ota/hw_iot_ota.h
+++ components/service/ota/hw_iot_ota.h
@@ -1,8 +1,11 @@
 #ifndef HW_IOT_OTA_H
 #define HW_IOT_OTA_H
 
+#include "esp_err.h"
 
+typedef void (*hw_iot_ota_finish_callback_t)(int code); // OTA完成回调函数
 
-
+esp_err_t hw_iot_ota_init(const char *url, hw_iot_ota_finish_callback_t cb); // 初始化OTA：设置OTA服务器URL
+esp_err_t hw_iot_ota_start(void);                                            // 启动OTA
 
 #endif
